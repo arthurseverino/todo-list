5cf71e4d76bec4572ff545efeb94c907fe0139e0
Arthur Severino <aas1181@nyu.edu>
2023-10-18 21:23:03 -0400

    atomic commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+/*
+
+add localstorage to make visualization testing easier
+the default when opened should show inbox 
+
+A button that says + Add Project / + Add Task placed right after the last task / project.
+When clicked, a modal pops up with an x on the top-right and an add button on the bottom just like library.
+
+when you add a task, you add the description of the task and date its due
+then you add it to inbox and check the date to also put it in this week or Today 
+
+when you add a project, you only ask for project name
+then you append a div to the left side of the screen with the title of project name that was entered
+Then, make that div clickable(button), and it will show an array of tasks when clicked - 
+these tasks get appended just like if they were being appended to inbox, with a modal form 
+
+to add task to project put a button called + Add Project Task
+
+these tasks are not added to inbox tho so keep two diff arrays:
+array of tasks in inbox, array of projects, array of tasks in projects
+
+you can also delete tasks and projects, simple as deleting from array (check library for how to delete)
+
+make sure all buttons stay active when clicked and hovered to show where you are 
+
+you can edit task by reopening form with the same data in it that it has 
+currently and change the green add button to a yellow "edit"
+
+everything in sidebar is a button
+
+*/
